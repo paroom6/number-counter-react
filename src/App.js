617045f44207d70bf2246389cfe34463dba59bfc
paroom6 <opwqer3729@gmail.com>
2024-02-19 18:32:18 +0900

    feat: vlpt 7장 과제

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  let [cnt, setCnt] = useState(0);
+  const incsCnt = () => {
+    console.log("+1");
+    setCnt(cnt + 1);
+  }
+  const decsCnt = () => {
+    console.log("-1");
+    setCnt(cnt - 1);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div>
+      <h1>{cnt}</h1>
+      <button onClick={incsCnt}>+1</button>
+      <button onClick={decsCnt}>-1</button>
     </div>
+    </>
   );
 }
 
 export default App;
+ 
